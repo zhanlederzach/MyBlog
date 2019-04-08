94253f3332937acec91d124770b4cd3b4ddd056e
Zhanel <miras_13@bk.ru>
2019-04-09 02:29:23 +0300

    Changed the HTML for the site.

---
+++ loops.py
@@ -0,0 +1,10 @@
+# def hi(name):
+#     print('Hi ' + name + '!')
+
+# girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+# for name in girls:
+#     hi(name)
+#     print('Next girl')
+
+for i in range(1, 6):
+    print(i)
